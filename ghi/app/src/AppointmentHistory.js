9a97becbaa3d9fa8fc9f19d252da2de01f948dcb
Caleb Lee <cleelee1126@gmail.com>
2022-12-08 19:11:49 -0800

    changed class based component to function based component for appointment history

--- ghi/app/src/AppointmentHistory.js
+++ ghi/app/src/AppointmentHistory.js
@@ -1,101 +1,98 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-class AppointmentHistory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      appointments: [],
-      vin: '',
-      submitted: false,
-      hasError: false,
-    };
+function AppointmentHistory() {
+    const [appointments, setAppointments] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredResults, setFilteredResults] = useState([]);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    useEffect(() => {
+        const fetchAppointments = async () => {
+            const url = "http://localhost:8080/api/appointments/";
+            const response = await fetch(url);
 
-  handleChange(event) {
-    const value = event.target.value;
-    this.setState({ [event.target.name]: value });
-  }
+            if (response.ok) {
+                const data = await response.json();
 
-  handleSubmit(event) {
-    event.preventDefault(); // prevent the default form submission behavior
-
-    this.setState({ submitted: true }); // set the submitted flag to true
-
-    // filter the appointments based on the vin and completed value
-    const appointments = this.state.appointments.filter(
-      appointment =>
-        appointment.vin === this.state.vin && appointment.completed === true
-    );
-
-    if (appointments.length === 0) { // check if the vin exists in the appointments
-      window.alert("Error, invalid VIN"); // display an error message
-      this.setState({ appointments: [], hasError: true });
-      return;
-    } else {
-        this.setState({ appointments, hasError: false }); // update the state with the filtered appointments
+                setAppointments(data.appointments);
+            }
+        };
+        fetchAppointments();
+        }, []);
+    const searchAppointments = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const filteredData = appointments.filter((appointment) => {
+                return Object.values(appointment).join('').toLowerCase().includes(searchInput.toLowerCase())
+            })
+            setFilteredResults(filteredData)
+        }
+        else{
+            setFilteredResults(appointments)
+        }
     }
-  }
-
-  async componentDidMount() {
-    const url = `http://localhost:8080/api/appointments/`;
-    let response = await fetch(url);
-    if (response.ok) {
-      let data = await response.json();
-      this.setState({ appointments: data.appointments });
-    }
-  }
-
-  catch (e) {}
-
-  render() {
     return (
-      <>
-        <h1>Appointment History</h1>
-        <form className="search-form" onSubmit={this.handleSubmit}>
-          <label className="search-label">
-            VIN:
+        <div className="container mt-3">
+        <div className="input-group">
             <input
-              type="text"
-              name="vin"
-              value={this.state.vin}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Search</button>
-        </form>
-        {this.state.submitted && (
-          <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>VIN</th>
-                    <th>Vehicle Owner</th>
-                    <th>Date & Time</th>
-                    <th>Reason</th>
-                    <th>Technician</th>
-                    <th>VIP</th>
-                </tr>
-            </thead>
-            <tbody>
-              {this.state.appointments.map(appointment => {
-                return (
-                  <tr key={appointment.id}>
-                    <td>{appointment.vin}</td>
-                    <td>{appointment.vehicle_owner}</td>
-                    <td>{appointment.date_time}</td>
-                    <td>{appointment.reason}</td>
-                    <td>{appointment.technician.name}</td>
-                    <td>{appointment.is_vip ? "Yes" : ""}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
-      </>
-    );
-  }
-}
+            type="search"
+            className="form-control rounded"
+            aria-describedby="search-addon"
+            placeholder="Search by VIN here"
+            aria-label="Search"
+            onChange={(e) => searchAppointments(e.target.value)}
+            value={searchInput} />
+            <button type="button"
+            className="btn btn-outline-primary"
+            onClick={(e) => searchAppointments(e.target.value)}
+            value={searchInput}>Search</button>
+        </div>
+
+        <h1>Service Appointments</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>VIN</th>
+                        <th>Vehicle Owner</th>
+                        <th>Date & Time</th>
+                        <th>Technician</th>
+                        <th>Reason</th>
+                        <th>VIP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchInput.length > 0 ? (
+                        filteredResults.map((appointment) => {
+                            return (
+                                <tr key={appointment.id}>
+                                    <td>{appointment.vin}</td>
+                                    <td>{appointment.vehicle_owner}</td>
+                                    <td>{appointment.date_time}</td>
+                                    <td>{appointment.technician.name}</td>
+                                    <td>{appointment.reason}</td>
+                                    <td>{appointment.is_vip ? "Yes" : ""}</td>
+
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        appointments.map((appointment) => {
+                            return (
+                                <tr key={appointment.id}>
+                                <td>{appointment.vin}</td>
+                                <td>{appointment.vehicle_owner}</td>
+                                <td>{appointment.date_time}</td>
+                                <td>{appointment.technician.name}</td>
+                                <td>{appointment.reason}</td>
+                                <td>{appointment.is_vip ? "Yes" : ""}</td>
+
+                            </tr>
+                        );
+                    })
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+
+};
 export default AppointmentHistory
